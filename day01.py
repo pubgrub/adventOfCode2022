1b8d3116de8133c76770cc4d8f2fc7a163c96077
Michael Fuchs <pubgrub@gmx.de>
2022-12-01 10:57:28 +0100

    Day 01 done

---
+++ day01.py
@@ -0,0 +1,27 @@
+# AdventOfCode 2022
+# Day 01
+
+#get input data
+lines = []
+with open( "01.data", "r") as file:
+    for line in file:
+        lines.append( line.strip())
+file.close()
+
+calories = 0
+maxCalories = [ 0, 0, 0]
+for l in lines:
+  if l:
+    calories += int( l)
+  else:
+    for i, m in enumerate( maxCalories):
+      if calories >= m:
+        maxCalories.insert( i, calories)
+        maxCalories.pop()
+        break
+    calories = 0
+
+#Task 1
+print( 'Result Task 1: ', maxCalories[0])
+
+print( 'Result Task 2', sum( maxCalories))
