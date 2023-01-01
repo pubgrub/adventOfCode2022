4cafed916253277a4436afb4361c6ec171e4bb86
Michael Fuchs <pubgrub@gmx.de>
2023-01-01 18:43:51 +0100

    work on 21

--- day21.py
+++ day21.py
@@ -2,7 +2,7 @@ import copy
 # AdventOfCode 2022
 
 DAY = '21'
-TEST =0
+TEST = 0
 
 #get input data
 testStr = 'test' if TEST else ''
@@ -28,35 +28,67 @@ for l in lines:
     toDoOrig[ s[0]] = ([ s[1], s[3], s[2]])
       
 
+def solve1( elem):
+  if elem in solved:
+    return solved[elem]
+  else:
+    if toDo[elem][2] == '+':
+      return( solve1( toDo[elem][0]) + solve1(toDo[elem][1]))
+    elif toDo[elem][2] == '-':
+      return( solve1( toDo[elem][0]) - solve1(toDo[elem][1]))
+    elif toDo[elem][2] == '*':
+      return( solve1( toDo[elem][0]) * solve1(toDo[elem][1]))
+    elif toDo[elem][2] == '/':
+      return( int(solve1( toDo[elem][0]) / solve1(toDo[elem][1])))
+
+def getAllCodes(lines):
+  codes = []
+  for l in lines:
+    s = l.replace( ":", "").split( ' ')
+    if len(s) == 2:
+      if not s[0] in codes:
+        codes.append( s[0])
+    else:
+      del s[2]
+      for i in s:
+        if not i in codes:
+          codes.append( i)
+  return codes
+
 # Task 1
 
 toDo = copy.deepcopy( toDoOrig)
 solved = copy.deepcopy( solvedOrig)
 
-done = False
-while not done:
-  toDelete = []
-  for k, v in toDo.items():
-    if not ( v[0] in solved  and v[1] in solved):
-       continue
-    else:
-      if v[2] == '+':
-        solved[k] = solved[ v[0]] + solved[ v[1]]
-      elif v[2] == '-':
-        solved[k] = solved[ v[0]] - solved[ v[1]]
-      elif v[2] == '*':
-        solved[k] = solved[ v[0]] * solved[ v[1]]
-      elif v[2] == '/':
-        solved[k] = int(solved[ v[0]] / solved[ v[1]])
-      toDelete.append( k)
-  for d in toDelete:
-    toDo.pop( d)
-  if 'root' in solved:
-    done = True
-
-print( 'Task 1: ', solved['root'])
+print( 'Task 1: ', solve1( 'root'))
 
 # Task 2
+
+toDo = copy.deepcopy( toDoOrig)
+solved = copy.deepcopy( solvedOrig)
+
+dep = ['humn']
+indep = getAllCodes( lines)
+
+# move dependent codes from indep to dep
+done = False
+while not done:
+  done = True
+  for d in dep:
+    for t in toDo:
+      if d in toDo[t] and t in indep:
+        dep.append( t)
+        indep.remove( t)
+        done = False
+
+
+
+
+print( len(indep), len(set(indep)))
+print( len(dep), len(set(dep)))
+print( dep)
+
+exit()
 
 humn = -1000
 done = False
