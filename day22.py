c5cdb29d0aac5b5bc0f47cc2cd83f75fdb9327a7
Michael Fuchs <pubgrub@gmx.de>
2023-01-30 10:09:49 +0100

    Day 22 Part 1

--- day22.py
+++ day22.py
@@ -2,7 +2,7 @@
 
 import re
 DAY = '22'
-TEST = 1
+TEST = 0
 
 #get input data
 testStr = 'test' if TEST else ''
@@ -14,32 +14,53 @@ with open( filename, "r") as file:
 		lines.append( line[:-1])
 file.close()
 
-xStart = []
-yStart = []
-xLen = []
-yLen = []
+
+steps = [(1,0),(0,1),(-1,0),(0,-1)]
+
+def getNewPos(pos, dir):
+  return (pos[0] + steps[dir][0], pos[1] + steps[dir][1]  )
+
+lines.insert(0,'')
+pOrder = re.compile('(\d+)([LR])')
+walkList = []
+turnValue = {'L': -1, 'R':1}
+l = lines.pop()
+for match in  pOrder.finditer(l):
+  walkList.append((int(match.group(1)), turnValue[match.group(2)])) 
+
+xStart = [0]
+yStart = [0]
+xLen = [0]
+yLen = [0]
 xWalls = {}
 yWalls = {}
 
-xMax = len(max(lines[:-2], key=len)) - 1
+xMax = len(max(lines, key=len)) + 1
+
+for i in range(0,len(lines)):
+  lines[i] = (' ' + lines[i] + ' ' * (xMax + 1))[:xMax + 1]
+
 cols = ['' for _ in range(xMax + 1)] 
+print( lines)
+
 
 p1 = re.compile('(\s*)([\.#]+)')
 p2 = re.compile('#')
-p3 = re.compile('(\d+)([LR])')
-for y, l in enumerate(lines[:-2]):
-  for x,c in enumerate( list(l + ' ' * xMax)[:xMax + 1]):
+for y, l in enumerate(lines):
+  for x,c in enumerate(l):
     cols[x] = cols[x] + c 
-  m1 = p1.match(l)
-  xStart.append(len(m1.group(1)))
-  xLen.append(len(m1.group(2)))
-  m2 = p2.finditer(m1.group(2))
-  if not y in xWalls:
-    xWalls[y] = []
-  for w in m2:
-    xWalls[y].append( w.start() + xStart[y])    
+  if y > 0 and y < len(lines) - 1:
+    m1 = p1.match(l)
+    xStart.append(len(m1.group(1)))
+    xLen.append(len(m1.group(2)))
+    m2 = p2.finditer(m1.group(2))
+    if not y in xWalls:
+      xWalls[y] = []
+    for w in m2:
+      xWalls[y].append( w.start() + xStart[y])    
 
-for x, l in enumerate(cols):
+
+for x, l in enumerate(cols[1:-1]):
   print('l: ', l)
   m1 = p1.match(l)
   yStart.append(len(m1.group(1)))
@@ -50,18 +71,39 @@ for x, l in enumerate(cols):
   for w in m2:
     yWalls[x].append( w.start() + yStart[x])    
 
-m1 = p3.finditer(lines[-1])
-
-for match in m1:
-  print( match.groups())
 
 
+dir = 0
 
-l = [ 0,1,2,3,4]
-for i in reversed(l):
-  print( i)
+pos = (xStart[1],1)
+print('pos: ',pos)
+for (dist,turn) in walkList:
+  wrap = False
+  while dist:
+    if wrap:
+      newPos = getNewPos(newPos,dir)
+      wrap = False
+    else:
+      newPos = getNewPos(pos,dir)
+    symbol = lines[newPos[1]][newPos[0]]     
+    if symbol == '.':
+      pos = newPos
+      print( 'pos: ', pos)
+      dist -= 1
+    elif symbol == '#':
+      dist = 0
+    elif symbol == ' ':
+      wrap = True
+      if dir == 0:
+        newPos = (xStart[pos[1]] - 1,pos[1])
+      elif dir == 1:
+        newPos = (pos[0],yStart[pos[0]] - 1)
+      elif dir == 2:
+        newPos = (xStart[pos[1]] + xLen[pos[1]],pos[1])
+      elif dir == 3:
+        newPos = (pos[0],yStart[pos[0]] + yLen[pos[0]])
+  dir = (dir + turn) % 4
 
-exit()
 
 
 
@@ -72,7 +114,9 @@ print( xStart, xLen)
 print( xWalls)
 print( yStart, yLen)
 print( yWalls)
-exit()
+print( pos, dir)
+print( pos[0] * 4 + pos[1] * 1000 + dir)
+
 
 
 
